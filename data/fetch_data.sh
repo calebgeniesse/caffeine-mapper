750cf8e982460b71a168943607b04665919c3e4b
calebgeniesse <calebgeniesse@gmail.com>
2018-12-11 11:17:12 -0800

    update fetch_data.sh, move to data

--- data/fetch_data.sh
+++ data/fetch_data.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# cd into data
+# get current work_dir
 work_dir=$(pwd)
-data_dir=data/myconnectome
+echo $work_dir
+
+# mkdir + cd "data/myconnectome" (if not there already)
+data_dir=$(dirname $0)/myconnectome
+echo $data_dir
+
 mkdir -p $data_dir
 cd $data_dir
+echo $(pwd)
 
 # fetch data
 wget -N -r -l inf --no-remove-listing -nH --cut-dirs=3 http://web.stanford.edu/group/poldracklab/myconnectome-data/base
 
 # cd to original working directory
 cd ${work_dir}
+echo $(pwd)
