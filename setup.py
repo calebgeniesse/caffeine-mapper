3beee7223d13efaba17bd3bdd64e1aef413e23b2
calebgeniesse <calebgeniesse@gmail.com>
2018-12-11 11:47:39 -0800

    make utils pip installable (pip install -e .)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='utils',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MyConnectome + Mapper',
+    author='calebgeniesse + RdoubleA ',
+    license='',
+)
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['load_data', 'utils']
+
+from load_data import *
+from utils import *
