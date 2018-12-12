278156a4b4b53eb9d10526d830fa87652d64dff4
calebgeniesse <calebgeniesse@gmail.com>
2018-12-11 23:44:32 -0800

    move old scripts to notebooks

---
+++ notebooks/load_data.py
@@ -0,0 +1,362 @@
+"""
+dataset loader.
+@author Caleb Geniesse
+"""
+from __future__ import print_function, division
+from __future__ import unicode_literals
+
+import os
+import re
+import glob
+from collections import defaultdict
+
+import logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+import numpy as np
+import pandas as pd 
+import scipy as sp 
+
+import scipy.stats 
+from sklearn.datasets.base import Bunch
+#from sklearn.preprocessing import LabelEncoder
+#from nilearn.input_data import NiftiLabelsMasker
+#from nilearn.image import load_img
+#from nilearn.signal import clean
+#from nilearn.datasets import fetch_atlas_msdl
+
+class config:
+    # walk back, until we find base directory
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    while len(base_dir) and not os.path.exists(os.path.join(base_dir, 'data')):
+        base_dir = os.path.dirname(base_dir)
+    # define some paths
+    data_dir = os.path.join(base_dir, 'data/myconnectome/base/')
+    data_scrubbed_dir = os.path.join(data_dir, 'combined_data_scrubbed')
+    data_tmask_dir = os.path.join(data_dir, 'rsfmri/tmasks')
+    data_behavior_dir = os.path.join(data_dir, 'behavior')
+    data_parcel_dir = os.path.join(data_dir, 'parcellation')
+
+
+
+def fetch_data(**kwargs):
+    """ Fetch my connectome data.
+        
+        Just print command to download data for now... too long to run in-script.
+    """
+    url = "http://web.stanford.edu/group/poldracklab/myconnectome-data/base/combined_data_scrubbed"
+    cmd = "wget -N -r -l inf --no-remove-listing -nH --cut-dirs=3 {}".format(url)
+    print("Run the following in a new Jupyter cell to fetch data:\n")
+    print("%%bash")
+    print("mkdir -p data")
+    print("cd data")
+    print("{}".format(cmd))
+    # import subprocess as sp
+    # o = sp.check_output(cmd, shell=True)
+    # 
+    return True
+
+
+
+##############################################################################
+### mask helpers
+##############################################################################
+def get_RSN_rmask(atlas, n=None, minor=False, ignore=['Zero', 'na'], **kwargs):
+    """ Return region mask based on RSNs
+    
+    Inputs
+    ------
+        :atlas = Bunch
+        :n = int, return rmask for first n networks
+        :minor = bool, return rmask for minor networks (major by default)
+    """
+    rmask = atlas.data.copy()
+
+    # get list of RSNs sorted by size
+    RSNs = rmask[~rmask.network.isin(ignore)].groupby('network')
+    RSNs = RSNs.network.count().sort_values(ascending=minor)
+    RSNs = RSNs.index.tolist()[:n]
+    
+    # define rmask based on RSNs
+    rmask = rmask.assign(data_id = rmask.index)
+    rmask = rmask.assign(rmask = rmask.network.isin(RSNs))
+    rmask = rmask.loc[rmask.rmask, ['data_id', 'region', 'network', 'rmask']]
+    #rmask = rmask.assign(group = rmask.network.map(RSNs.index))
+    return rmask
+
+
+
+def get_session_tmask(meta, session=None, **kwargs):
+    """ Return temporal mask for subject(s)
+
+    Inputs
+    ------
+        :data = data to mask 
+        :session = subcode (load all by default, assumes data for all sessions)
+    """
+    def glob_tmask(subcode=None):
+        subcode = 'sub???' if subcode is None else subcode.split('.txt')[0]
+        glob_str = os.path.join(config.data_tmask_dir, subcode + '.txt')
+        found = sorted(glob.glob(glob_str))
+        return found
+    
+    def load_tmask(filename):
+        tmask = pd.read_csv(filename, header=None, names=['tmask'], dtype=bool)
+        tmask = tmask.assign(session=os.path.basename(filename).split('.txt')[0]) 
+        tmask = tmask.assign(tr_id=tmask.index)
+        return tmask
+    
+    # glob tmask paths (subject specific)
+    tmask_paths = sorted(__ for _ in np.ravel(session) for __ in glob_tmask(_))
+    
+    # load tmasks
+    tmask = pd.concat(map(load_tmask, tmask_paths), ignore_index=True, sort=False)
+    
+    # joint to meta
+    tmask = meta.copy().reset_index(drop=False).join(
+        tmask.set_index(['session', 'tr_id']), 
+        how='inner', on=['session', 'tr_id'],
+        )
+    
+    # assign data_id, return 
+    tmask = tmask.fillna(False).assign(data_id = tmask.index)
+    tmask = tmask.loc[tmask.tmask, ['data_id', 'session', 'tr_id', 'tmask']]
+    return tmask
+
+
+
+##############################################################################
+### meta data helper functions
+##############################################################################
+def load_atlas(atlas_file=None):
+    """ Load parcellation / atlas data.
+    """ 
+    if atlas_file is None:
+        atlas_file = os.path.join(config.data_parcel_dir, "parcel_data.txt")
+    
+    # load parcellation into DataFrame
+    df_parcel = pd.read_table(atlas_file, header=None)
+
+    # relabel columns
+    df_parcel = df_parcel.rename(
+        columns={
+            0:'target',
+            1:'hemisphere',
+            2:'x',
+            3:'y',
+            4:'z',
+            5:'region',
+            6:'subregion',
+            7:'network'
+        })
+
+    # region_coords: (x, y, z)
+    df_coords = df_parcel[['x', 'y', 'z']].copy()
+
+    # regions: string list of region labels
+    df_regions = df_parcel[['region']].copy()
+
+    # networks: names of the networks
+    df_networks = df_parcel[['network']].copy()
+    
+    # Bunch
+    atlas = Bunch(
+        data=df_parcel,
+        regions=df_regions,
+        region_coords=df_coords,
+        networks=df_networks,
+        )
+    return atlas
+
+
+
+def clean_meta(df, columns=None, zscore=False, **kwargs):
+    """ Clean meta DataFrame, zscore.
+    """
+    # copy input df
+    df_clean = df.copy()
+    
+    # select columns
+    if columns is not None:
+        df_clean = df_clean[columns]
+    
+    # replace non-numerics
+    df_clean = df_clean.replace('.', 0.0)
+    df_numeric = df_clean.astype(str).applymap(str.isnumeric)
+    df_clean[df_numeric==False] = np.nan
+    df_clean = df_clean.fillna(0.0)
+
+    # only keep numeric data, convert to float
+    df_clean = df_clean.astype(float)
+    
+    # z-score values
+    if zscore is True:
+        good_rows = df_clean.any(axis=1) & df_clean.std(axis=1).gt(0)
+        good_cols = df_clean.any(axis=0) & df_clean.std(axis=0).gt(0)
+        df_nonzero = df_clean.loc[good_rows, good_cols]
+        df_clean.loc[good_rows, good_cols] = scipy.stats.zscore(df_nonzero, axis=0)
+    
+    # fill nans
+    df_clean = df_clean.fillna(0.0)
+    
+    # return cleaned
+    return df_clean
+
+
+
+##############################################################################
+### main loaders
+##############################################################################
+def load_scrubbed(**kwargs):
+    """ Loads scrubbed data
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('load_scrubbed(**{})'.format(kwargs))
+    
+    # file path (avmovie only, for now)
+    glob_str = os.path.join(config.data_scrubbed_dir, "sub???.txt")
+    data_paths = sorted(glob.glob(glob_str))
+    tmask_paths = [os.path.join(config.data_tmask_dir, os.path.basename(_)) for _ in data_paths]
+
+    glob_str = os.path.join(
+        config.data_behavior_dir, 'trackingdata_goodscans.txt')
+    meta_paths = sorted(glob.glob(glob_str))
+
+    # hacky, but just print command to fetch data, if not already
+    if len(data_paths) < 1:
+        fetch_data()
+        return None
+    
+    # how many sessions to load?
+    n_sessions = kwargs.get('n_sessions', -1)
+    if n_sessions == -1:
+        n_sessions = len(data_paths)
+
+    # check sizes
+    logger.debug('found {} data files'.format(len(data_paths)))
+    logger.debug('found {} tmask files'.format(len(tmask_paths)))
+    logger.debug('found {} meta files'.format(len(meta_paths)))
+    logger.debug('using {} sessions'.format(n_sessions))
+
+    # load data ?
+    logger.info("Loading data...")
+    dataset = []
+    for i, data_path in enumerate(data_paths):
+
+        if i >= n_sessions:
+            break
+
+        logging.info("  [+] session: {}, file: {}".format(i, os.path.basename(data_path)))
+        
+        # load dataframe
+        df_data = pd.read_csv(data_path, header=None, delim_whitespace=True)
+
+        # load meta as tr_id (for now...)
+        df_meta = df_data.assign(tr_id = df_data.index.values)[['tr_id']]
+
+        # parse session, session_id from file
+        session = os.path.basename(data_path).split('.txt')[0]
+        session_id = int(''.join([__ for __ in session if __.isdigit()]))
+        df_meta = df_meta.assign(session=session, session_id=session_id)
+        
+        # join with other meta files
+        df_meta = df_meta.join(
+            pd.concat(pd.read_table(_,index_col='subcode') for _ in meta_paths)
+            , how='left', on='session'
+            )
+           
+
+        # load atlas
+        atlas = load_atlas()
+
+
+        # load tmask (subject specific)
+        df_tmask = get_session_tmask(df_meta, session=session, **kwargs.get('tmask_kwds', {}))
+        if kwargs.get('apply_tmask'):
+            df_data = df_data.loc[df_tmask.data_id, :]
+            df_meta = df_meta.loc[df_tmask.data_id, :]
+            logging.info("      keeping: {} (time points)".format(df_data.shape[0]))
+
+        # load rmask (region specific)
+        df_rmask = get_RSN_rmask(atlas, **kwargs.get('rmask_kwds', {}))
+        if kwargs.get('apply_rmask'):
+            df_data = df_data.loc[:, df_rmask.data_id]
+            logging.info("      keeping: {} (regions)".format(df_data.shape[-1]))
+
+
+         # clean data, meta
+        #df_data = clean_data(data=df_data, meta=df_meta, **kwargs)
+        #df_meta = clean_meta(df_meta, **kwargs)
+      
+        # z score (?)
+        if kwargs.get('zscore'):
+            logging.info("      zscore: {}".format(kwargs.get('zscore')))
+            df_data.loc[:] = scipy.stats.zscore(df_data.values, axis=0)
+
+
+        #masker = NiftiLabelsMasker(
+        #    labels_img=atlas_paths[0],
+        #    memory="nilearn_cache"
+        #    )
+        #masker = masker.fit()
+
+        # low pas filter
+        #cleaned_ = clean(df_data.values,
+        #    low_pass=0.09, high_pass=0.008
+        #    )
+        #df_data.iloc[:, :] = cleaned_
+
+        # reset meta index 
+        df_meta = df_meta.set_index(['session', 'session_id', 'tr_id'])
+
+      
+        # save masker, x
+        dataset.append(Bunch(
+            data=df_data.copy().fillna(0.0),
+            meta=df_meta.copy().fillna(0),
+
+            #masker=masker,
+            X=df_data.values.copy(),
+            y=df_meta.values.copy(),
+            
+            # masks
+            tmask=df_tmask.copy(),
+            rmask=df_rmask.copy(),
+
+            # atlas
+            atlas=atlas,
+            ))
+    
+    
+    # return dataset as Bunch
+    if kwargs.get('merge') is False:
+        for i, session in enumerate(dataset):
+            meta = (clean_meta(session.meta, **kwargs)
+                            .reset_index(drop=False)
+                            )
+            dataset[i].meta = meta.copy()
+            dataset[i].y = meta.set_index('session').values.copy()
+        return dataset
+
+    # nerge data into single dataframe, array, etc
+    dataset = Bunch(
+        data=pd.concat((_.data for _ in dataset), ignore_index=True, sort=False).fillna(0.0),
+        meta=pd.concat((_.meta for _ in dataset), ignore_index=False, sort=False),
+        tmask=pd.concat((_.tmask for _ in dataset), ignore_index=False, sort=False),
+        rmask=dataset[0].rmask,
+        atlas=dataset[0].atlas,
+        )
+    dataset.meta = (clean_meta(dataset.meta, **kwargs)
+                    .reset_index(drop=False)
+                    #.set_index('session')
+                    )
+
+
+    dataset.X = dataset.data.values.reshape(-1, dataset.data.shape[-1])
+    dataset.y = dataset.meta.values.reshape(-1, dataset.meta.shape[-1])
+    return dataset
+
+
+
+   
---
+++ notebooks/utils.py
@@ -0,0 +1,268 @@
+"""
+Some utils for caffeine-mapper...
+"""
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
+import dyneusr as ds
+import kmapper as km
+import networkx as nx
+import bct
+
+import numpy as np
+import pandas as pd
+import scipy.stats
+
+from sklearn.datasets.base import Bunch
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS, TSNE
+
+from umap.umap_ import UMAP
+from hdbscan import HDBSCAN
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from collections import Counter, OrderedDict
+from functools import partial
+from itertools import product
+
+from load_data import *
+
+##############################################################################
+### helper functions
+##############################################################################
+def get_data_splits(data_, zscore=True, groupby='day_of_week', groups=None, **kwargs):
+    """ Return data splits.
+    
+    Inputs
+    ------
+        :data_ = Bunch,  merged sessions 
+        :groups = dict, {group: name} mapping of splits
+        
+    """
+    x_ = data_.X.copy()
+    
+    # z-score
+    if zscore is True:
+        x_ = scipy.stats.zscore(x_, axis=0)
+
+    # group by fed / fasted
+    grouped = data_.meta.groupby(groupby)
+    
+    # get splits    
+    splits = dict()
+    for g_i, (group, df_group) in enumerate(grouped):
+        
+        # extract data for fed / fasted
+        x_group = x_[df_group.index, :]
+
+        # extract RSNs data for fed / fasted
+        df_rsn_group = get_RSN_act(x_group, data_.rmask, **kwargs)
+
+        # name split
+        try:
+            name = '{:}_{:}'.format(groupby, str(g_i).zfill(len(str(grouped.ngroups))))
+            name = groups.get(group) or groups.get(int(group)) or name
+        except Exception as e:
+            pass
+            
+        # save split
+        splits[name] = split = Bunch(
+            meta=df_group.copy(),
+            X=x_group.copy(),
+            RSN=df_rsn_group.copy(),
+            group=group,
+            name=name,
+            )
+    
+        # print shapes
+        print("{:15} => {:15}  x.shape: {}  RSN.shape: {}".format(
+            group, name, split.X.shape, split.RSN.shape
+        ))
+    
+    # return as Bunch
+    splits = Bunch(**splits)
+    return splits
+
+
+
+def get_RSN_act(x, rsn, zscore=True, density=None, threshold=0.05, binary=True):
+    """ Compute mean activity for RSN at each TR.
+    
+    Inputs
+    ------
+        :x = np.ndarray (TR, ROI)
+        :rsn = pd.DataFrame (ROI, RSN)
+        :zscore = bool, whether or not to zscore x
+        :density = float, set everything below 1-density to 0
+        :threshold = float, set everything above threshold to 1 (set density=1.0)
+        :binary = bool, whether or not to binarize results
+        
+    """
+    x_ = x.copy() 
+    rsn_ = rsn.copy()
+    if 'networks' in rsn_:
+        rsn_ = dict(rsn_).get('networks')
+    if isinstance(rsn_, pd.Series):
+        rsn_ = rsn_.to_frame()
+
+    # reset indices (i.e. if rmask was applied to data...)
+    if rsn_.shape[0] > x_.shape[-1]:
+        rsn_ = rsn_.reset_index(drop=True)
+    elif rsn_.index.max() > x_.shape[-1]:
+        rsn_ = rsn_.reset_index(drop=True)
+
+    # z-score (?)
+    if zscore is True:
+        x_ = scipy.stats.zscore(x_, axis=0)
+
+    # get average RSN activity for each network
+    #       sorted by -len(ROIs), -sum(ROIs) 
+    # TODO: this could be its own function
+    rsn_rois = rsn_.groupby('network').indices.items()
+    rsn_rois = sorted(rsn_rois, key=lambda _: [-len(_[-1]), -sum(_[-1])])
+    rsn_act = {rsn: x_[:, rois].mean(axis=1) for (rsn, rois) in rsn_rois}
+
+    # save as DataFrame
+    df_rsn_act = pd.DataFrame(rsn_act)
+    
+    # threshold / density (?)
+    if density is not None:
+        threshold = df_rsn_act.quantile(1.0-density, axis=0)
+        df_rsn_act[df_rsn_act.lt(threshold)] = 0
+
+    # threshold (?)
+    if threshold is None:
+        threshold = df_rsn_act.mean(axis=0)
+
+    # binary (?)
+    if binary is True:
+        df_rsn_act = (df_rsn_act >= threshold).astype(int)
+ 
+    return df_rsn_act
+
+
+
+
+def get_PC(df, columns=None):
+    """ Find PC with max weight assigned to column.
+    """
+    # extract X
+    X = df.values
+    
+    # fit transform PCA
+    from sklearn.decomposition import PCA
+    pca = PCA()
+    U = pca.fit_transform(X)
+    V = pca.components_
+    
+    # create new DataFrame of components (V)
+    df_V = pd.DataFrame(V, columns=df.columns)
+    df_V.index = df_V.index.map('PC_{}'.format)
+   
+    # create new DataFrame of embedding (U)
+    df_U = pd.DataFrame(U, index=df.index, columns=df_V.index)
+    
+    # set column to 0,1 or idxmax   
+    if columns is None:
+        columns = pd.Series(df_U.columns[:2])
+    else:
+        columns = [_ for _ in np.ravel(columns)]
+        columns = df_V[columns].idxmax()   
+
+    # return PC for col
+    df_PC = df_U[columns]
+    df_PC.columns = ["{} [{}]".format(columns[_],_) for _ in columns.index]
+    return df_PC
+
+
+
+
+def run_mapper(X=None, y=None, X_inverse=True, lens=None, verbose=0, **params):
+    """ Wrap KeplerMapper calls
+    
+    Notes
+    -----
+    - See PCA_metadata.ipynb
+
+    """
+    # init MAPPER params
+    projection = params.get('projection', TSNE(perplexity=50, init='random', random_state=0))
+    clusterer = params.get('clusterer', HDBSCAN(allow_single_cluster=True))
+    cover = params.get('cover', km.Cover(10, 0.67))
+    X_inverse = X if X_inverse is True else X_inverse
+    
+    # fit
+    if lens is None:
+        mapper = km.KeplerMapper(verbose=verbose-1)
+        lens = mapper.fit_transform(X, projection=projection)
+       
+    # map
+    mapper = km.KeplerMapper(verbose=verbose)
+    graph = mapper.map(lens, X_inverse, clusterer=clusterer, coverer=cover)
+  
+    # dG
+    dG = ds.DyNeuGraph()
+    dG.fit(graph, y=y)
+    
+    # save results
+    results = Bunch(
+        X=X.copy(), X_inverse=X, 
+        lens=lens.copy(), graph=graph, 
+        params=params, cover=cover,
+        dG=dG, G=dG.G_, TCM=dG.tcm_.copy()
+    )
+    return results
+    
+
+    
+
+def draw_G(G, y=None, pos=None, ax=None, **kwargs):
+    """ Draw networkx graph.
+
+    Notes
+    -----
+    - See PCA_metadata.ipynb
+    
+    """
+    
+    # size by number of members
+    node_size = kwargs.get('node_size')
+    if node_size is None:
+        node_size = [1 * len(G.nodes[n]['members'])**1.5 for n in G]
+        kwargs.update(node_size=node_size)
+
+    
+    # color nodes by mode
+    node_color = kwargs.get('node_color')
+    if node_color is None and y is not None:
+        node_color = [Counter(y[_]).most_common()[0][0] for n,_ in G.nodes('members')]
+        kwargs.update(node_color=node_color)
+    
+    # color nodes by mode
+    layout = kwargs.get('layout')
+    if pos is None and layout is not None:
+        pos = layout(G)
+    
+    # plot
+    _ = nx.draw_networkx(
+        G, pos=pos,
+        with_labels=False, 
+        ax=ax,
+        **kwargs
+        )
+    
+    # remove spines
+    for spine in ax.spines:
+        ax.spines[spine].set_visible(False)
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    return ax
+    
